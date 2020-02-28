920e2aa5caa19cd5f41e4d6d0630f0cc2c7909e9
August Carlsson <august.carlsson@hotmail.com>
2020-02-28 16:58:56 +0100

    Renskriven

--- src/alda/graph/MyUndirectedGraph.java
+++ src/alda/graph/MyUndirectedGraph.java
@@ -48,22 +48,19 @@ public class MyUndirectedGraph<T> implements alda.graph.UndirectedGraph<T> {
 
     @Override
     public boolean add(T newNode) {
-        if(map.containsKey(newNode))
-            return false;
-        Node<T> node = new Node<>(newNode);
-        edges.put(newNode, new ArrayList<Edge<T>>());
-        from = (from == null) ? newNode:from;
-        map.put(newNode,node);
-        return true;
-
-
+        if(!map.containsKey(newNode)){
+            Node<T> node = new Node<>(newNode);
+            edges.put(newNode, new ArrayList<Edge<T>>());
+            from = (from == null) ? newNode:from;
+            map.put(newNode,node);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean connect(T node1, T node2, int cost) {
-        if(cost <= 0)
-            return false;
-        if(map.containsKey(node1) && map.containsKey(node2) ) {
+        if(map.containsKey(node1) && map.containsKey(node2) && cost > 0 ) {
             map.get(node1).neighbour.put(node2, cost);
             map.get(node2).neighbour.put(node1, cost);
             ArrayList<Edge<T>> fromList = edges.get(node1);
@@ -81,17 +78,16 @@ public class MyUndirectedGraph<T> implements alda.graph.UndirectedGraph<T> {
 
     @Override
     public boolean isConnected(T node1, T node2) {
-        Node n1 = map.get(node1);
-        return (n1 != null) && n1.neighbour.containsKey(node2);
+        return map.get(node1).neighbour.containsKey(node2);
     }
 
     @Override
     public int getCost(T node1, T node2) {
-        Integer value = null;
-        if(map.get(node1) != null)
-            value = map.get(node1).neighbour.get(node2);
-        return (value != null) ? value : -1;
-
+        Node<T> n = map.get(node1);
+        Integer cost = null;
+        if(n!=null)
+            cost = n.neighbour.get(node2);
+        return (cost != null) ? cost : -1;
     }
 
     @Override
@@ -103,19 +99,15 @@ public class MyUndirectedGraph<T> implements alda.graph.UndirectedGraph<T> {
     }
 
     private void depthFirst(T start, T end, Stack<T> path, HashSet<T> tried){
-        Node<T> current = map.get(start);
+        System.out.println(path);
         tried.add(start);
         path.push(start);
-        Iterator<Map.Entry<T, Integer>> children = current.neighbour.entrySet().iterator();
-        if(tried.contains(end))
-            return;
-        while(children.hasNext()){
-            Map.Entry<T, Integer> currentNode = children.next();
-            if(tried.contains(end))
+        for (Map.Entry<T, Integer> currentNode : map.get(start).neighbour.entrySet()) {
+            if (tried.contains(end))
                 return;
-            if(!tried.contains(currentNode.getKey()))
+            if (!tried.contains(currentNode.getKey()))
                 depthFirst(currentNode.getKey(), end, path, tried);
-            if(tried.contains(end))
+            if (tried.contains(end))
                 return;
         }
         path.pop();
@@ -125,23 +117,22 @@ public class MyUndirectedGraph<T> implements alda.graph.UndirectedGraph<T> {
     @Override
     public List<T> breadthFirstSearch(T start, T end) {
         HashMap<T, T> parentData = new HashMap<>();
-        LinkedList<T> currentNodeId = new LinkedList<>();
+        LinkedList<T> currentNodeData = new LinkedList<>();
         parentData.put(start, null);
-        currentNodeId.addLast(start);
+        currentNodeData.addLast(start);
+        T currentData;
 
-        T currentId;
-        while(!currentNodeId.isEmpty()){
-            T tempChildId;
-            currentId = currentNodeId.removeFirst();
-            currentNodeId.addAll(currentNodeId.size(), map.get(currentId).neighbour.keySet());
+        while(!currentNodeData.isEmpty()){
+            T tempChildData;
+            currentData = currentNodeData.removeFirst();
+            currentNodeData.addAll(currentNodeData.size(), map.get(currentData).neighbour.keySet());
             LinkedList<T> children = new LinkedList<>();
-            children.addAll(map.get(currentId).neighbour.keySet());
+            children.addAll(map.get(currentData).neighbour.keySet());
             while(!children.isEmpty()){
-                tempChildId = children.removeFirst();
-                if(!parentData.containsKey(tempChildId))
-                    parentData.put(tempChildId, currentId);
-
-                if(tempChildId.equals(end))
+                tempChildData = children.removeFirst();
+                if(!parentData.containsKey(tempChildData))
+                    parentData.put(tempChildData, currentData);
+                if(tempChildData.equals(end))
                     return shortestPath(parentData, start, end);
             }
         }
@@ -152,9 +143,8 @@ public class MyUndirectedGraph<T> implements alda.graph.UndirectedGraph<T> {
         List<T> shortestPath = new LinkedList<>();
         shortestPath.add(0, end);
         T next = end;
-        if(parentData.get(end) != null)
-            while((next = parentData.get(next)) != null)
-                shortestPath.add(0, next);
+        while((next = parentData.get(next)) != null)
+            shortestPath.add(0, next);
         return shortestPath;
 
     }
@@ -170,7 +160,7 @@ public class MyUndirectedGraph<T> implements alda.graph.UndirectedGraph<T> {
         PriorityQueue<Edge<T>> priorityQueue = new PriorityQueue<Edge<T>>();
         alda.graph.UndirectedGraph graph = new MyUndirectedGraph();
 
-        depthSearch(this, from, tried);
+        depthSearch(from, tried);
         for(T n : tried){
             distances.put(n, Integer.MAX_VALUE);
             determined.put(n, false);
@@ -181,10 +171,10 @@ public class MyUndirectedGraph<T> implements alda.graph.UndirectedGraph<T> {
         for(T data : tried){
             graph.add(data);
         }
-        return make(from, tried, pre, distances, determined, priorityQueue, graph);
+        return makeGraph(from, pre, distances, determined, priorityQueue, graph);
     }
 
-    private alda.graph.UndirectedGraph<T> make(T from, Set<T> visited, Map<T, T> pre, Map<T, Integer> distance, Map<T, Boolean> determined, PriorityQueue<Edge<T>> priorityQueue, alda.graph.UndirectedGraph<T> graph) {
+    private alda.graph.UndirectedGraph<T> makeGraph(T from, Map<T, T> pre, Map<T, Integer> distance, Map<T, Boolean> determined, PriorityQueue<Edge<T>> priorityQueue, alda.graph.UndirectedGraph<T> graph) {
         Edge<T> edge;
         for (Edge<T> e : this.getEdges(from)) {
             if (!determined.get(e.getDestination())) {
@@ -193,7 +183,6 @@ public class MyUndirectedGraph<T> implements alda.graph.UndirectedGraph<T> {
                     priorityQueue.add(e);
                     distance.put(e.getDestination(), p);
                 }
-
             }
         }
         edge = priorityQueue.poll();
@@ -204,16 +193,17 @@ public class MyUndirectedGraph<T> implements alda.graph.UndirectedGraph<T> {
             pre.put(edge.getDestination(), from);
             graph.connect(from, edge.getDestination(), edge.getWeight());
         }
-        return make(edge.getDestination(), visited, pre, distance, determined, priorityQueue, graph);
+        return makeGraph(edge.getDestination(), pre, distance, determined, priorityQueue, graph);
 
     }
 
-    private void depthSearch(MyUndirectedGraph<T> map, T from, Set<T> visited){
-        visited.add(from);
+    //Får köra en depth med edges, vet ej hur vi löser minimumSpanningTree utan edge klass.
+    private void depthSearch( T from, Set<T> tried){
+        tried.add(from);
         for(Edge<T> e : edges.get(from)){
             T destination = e.getDestination();
-            if(!visited.contains(destination))
-                depthSearch(map, destination, visited);
+            if(!tried.contains(destination))
+                depthSearch( destination, tried);
         }
     }
 
